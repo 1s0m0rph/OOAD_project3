0e935895bc9b29500cea0746d46d6dd93b24ca71
Corey Stephens <cost2833@colorado.edu>
2019-10-18 16:52:57 -0600

    Added print for active rentals to main

--- src/toolshop/Main.java
+++ src/toolshop/Main.java
@@ -17,16 +17,28 @@ public class Main
 		{
 			ArrayList<RentalRecord> completedRentals = store.incrementDay();
 			System.out.printf("\nDay %d\n", store.getCurrentDay());
+
+			// print completed records
+			System.out.println("COMPLETED RENTALS");
 			for (RentalRecord rr : completedRentals)
 			{
 				System.out.println(rr);
 			}
+
+			// rent new tools
 			int numCustomers = r.nextInt(customers.poolCount());
 			for (int j=0; j < numCustomers; j++)
 			{
 				Customer c = (Customer) customers.get();
 				c.rentTools();
 				customers.release(c);
+			}
+
+			// print active rentals
+			System.out.println("ACTIVE RENTALS");
+			for (RentalRecord rr : store.getRentalRecords())
+			{
+				System.out.println(rr);
 			}
 			System.out.printf("$%d in revenue today\n", store.getDailyRevenue());
 		}
