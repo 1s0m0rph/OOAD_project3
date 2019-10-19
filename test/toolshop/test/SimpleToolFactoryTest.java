f285f3fe3fe4af13050a94401b0e9e76cba64b65
Daniel Taylor <Isomorph@protonmail.com>
2019-10-18 18:38:22 -0600

    added some tests

---
+++ test/toolshop/test/SimpleToolFactoryTest.java
@@ -0,0 +1,36 @@
+package toolshop.test;
+
+import org.junit.jupiter.api.Test;
+import toolshop.*;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleToolFactoryTest
+{
+	
+	@Test
+	void createTool()
+	{
+		SimpleToolFactory stf = new SimpleToolFactory();
+		Tool t0 = stf.createTool("concrete");
+		assert(t0.getCategory() instanceof Concrete);
+		Tool t1 = stf.createTool("painting");
+		assert(t1.getCategory() instanceof Painting);
+		Tool t2 = stf.createTool("plumbing");
+		assert(t2.getCategory() instanceof Plumbing);
+		Tool t3 = stf.createTool("woodwork");
+		assert(t3.getCategory() instanceof Woodwork);
+		Tool t4 = stf.createTool("yardwork");
+		assert(t4.getCategory() instanceof Yardwork);
+		
+		//error checking
+		try
+		{
+			stf.createTool("this category does not exist");
+			assert(false);
+		}
+		catch (Exception ignored){}
+	}
+}
